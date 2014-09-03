0a7a760cca38d92e2baefa33d1fdb85c12ddcb48
mari-linhares <mariannelinhares@outlook.com>
2014-09-03 00:20:40 -0300

    Made Credits

--- screen/Credits.py
+++ screen/Credits.py
@@ -10,7 +10,13 @@ class Credits(object):
         self.credits_list.addButton(0, "arrow_back.png", data.config.WIDTH * 0.1, data.config.HEIGHT * 0.1, mask="arrow_leftMask.png")
 
     def displayOutput(self, display):
-        display.blit(data.getResource("credits.png"), (0, 0))
+        display.fill((34,177,76))
+        display.blit(data.getResource("credits.png"),(data.config.HEIGHT * 0.1,data.config.HEIGHT * 0.1))
+        
+        graphics.drawText(display, data.translate("credits"),data.config.WIDTH * 0.5, data.config.HEIGHT * 0.1,size=50, formatting ="center")
+        graphics.drawText(display, data.translate("students"),data.config.WIDTH * 0.5, data.config.HEIGHT * 0.31,size=30, formatting ="center")
+        graphics.drawText(display, data.translate("professors"),data.config.WIDTH * 0.5, data.config.HEIGHT * 0.42,size=30, formatting ="center")
+        graphics.drawText(display, data.translate("monitor"),data.config.WIDTH * 0.5, data.config.HEIGHT * 0.545,size=30, formatting ="center")
         self.credits_list.draw(display)
 
     def respondToUserInput(self, event):
